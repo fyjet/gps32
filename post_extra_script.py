796b655fb37cd2f0d1f5e383b572d14472d04f4d
Fyjet <bertrand@optiplex.intranet.loc>
2020-01-12 18:19:16 +0100

    change build process

--- post_extra_script.py
+++ post_extra_script.py
@@ -4,4 +4,5 @@ from shutil import copyfile
 # copy User_Setup.h configuration to downloaded library
 projectDir=env.get('PROJECT_DIR')
 print(projectDir)
+print(">>>> Copy User_Setup.h")
 copyfile(projectDir+"/include/User_Setup.h", projectDir+"/.pio/libdeps/esp32dev/TFT_eSPI_ID1559/User_Setup.h")
